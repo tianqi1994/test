c2e209693ab8eff3350d441ee6f95ec1b1d39138
麒 田 <1354636584@qq.com>
2020-04-23 16:35:11 +0800

    fisrtTest

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("testCommit");
-            Console.WriteLine("testCommit");
+            Console.WriteLine("testBranch");
         }
     }
 }
